81777e1730703c1eeb7e195dc7726ef0bc7e0272
Julian Riccelli <jriccelli@frba.utn.edu.ar>
2026-08-23 18:08:43 -0300

    2do test integracion

--- labs/laboratorio-testing-c/tests/test_integracion.c
+++ labs/laboratorio-testing-c/tests/test_integracion.c
@@ -31,13 +31,28 @@ void test_compra_con_descuento(void) {
  *  PARTE E — Disenar un test propio (ver README.md, Parte 9)
  * ═══════════════════════════════════════════════════════════════════════════ */
 
-/* TODO: escribir test_agregar_hasta_llenar() */
+/* TODO: */
+ void test_agregar_hasta_llenar(void) {
+    printf("\n[agregar hasta llenar]\n");
+    Carrito c;
+    carrito_init(&c);
+    Producto p = {"Leche", 350, 1};
+
+    carrito_agregar(&c, p);
+    carrito_agregar(&c, p);
+    carrito_agregar(&c, p);
+    carrito_agregar(&c, p);
+
+    ASSERT_IGUAL(MAX_ITEMS, carrito_contar(&c));
+    ASSERT_IGUAL(0, carrito_agregar(&c, p));
+    ASSERT_IGUAL(MAX_ITEMS, carrito_contar(&c));
+}
 
 int main(void) {
     printf("=== Tests de integracion ===");
     /* Descomentar a medida que agregues las funciones: */
-     test_compra_con_descuento(); 
-    /* test_agregar_hasta_llenar();  */
+    test_compra_con_descuento(); 
+    test_agregar_hasta_llenar();  
     RESUMEN();
     return EXIT_CODE();
 }
